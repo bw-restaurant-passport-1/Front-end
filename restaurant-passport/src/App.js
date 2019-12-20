2fee316cd9fd4995c3244cb2aae8e20aade8c6cf
adrianbparra <adrianplane12@gmail.com>
2019-12-19 20:34:15 -0700

    added component that are making the dashboard

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Dashboard from "./components/Dashboard/Dashboard.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Dashboard />
+      {/* <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ restaurant-passport/src/components/AddedRestaurants/AddedRestaurants.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Restaurant from "../Restaurant/Restaurant.js";
+
+
+const AddedRestaurants = () => {
+
+    
+
+    return (
+
+        <div>
+
+        </div>
+    )
+
+}
+
+export default AddedRestaurants;
