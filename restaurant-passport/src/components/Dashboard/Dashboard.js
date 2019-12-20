14c40e69adefead47e02afa05d4163e020674e4b
adrianbparra <adrianplane12@gmail.com>
2019-12-19 21:26:43 -0700

    Added the route to dashboard

--- restaurant-passport/src/components/Dashboard/Dashboard.js
+++ restaurant-passport/src/components/Dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Navbar from "../Navbar/Navbar.js";
 import Profile from "../Profile/Profile.js";
 import AddedRestaurants from "../AddedRestaurants/AddedRestaurants.js";
+
+
 //Where data is held for state 
 
 
