4c2f2924116512fbd50924ccb09721e54933f578
Freddie <fthompson803@live.com>
2019-12-20 01:03:24 -0500

    adding login page to store and login success

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import restaurantLoginPage from './components/restaurantLoginPage';
+import RestaurantLoginPage from './components/restaurantLoginPage';
 import restaurantCreateAccountPage from './components/restaurantCreateAccoutPage';
 import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<Route exact path='/' component={restaurantLoginPage} />
-			<Route path='/register' component={restaurantCreateAccountPage} />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Route exact path='/' component={RestaurantLoginPage} />
+      <Route path='/register' component={restaurantCreateAccountPage} />
+    </div>
+  );
 }
 export default App;
