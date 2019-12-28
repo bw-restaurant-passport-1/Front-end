9c7b10377c7dcd8a14b7610f059b74f9fbaf7ab7
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-27 23:27:33 -0500

    dark mode, get requests, routes, and footer done

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import RestaurantLoginPage from './components/restaurantLoginPage';
-import RestaurantCreateAccountPage from './components/restaurantCreateAccountPage';
+import RestaurantLoginPage from './components/accounts/restaurantLoginPage';
+import RestaurantCreateAccountPage from './components/accounts/restaurantCreateAccountPage';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Dashboard from "./components/Dashboard/Dashboard.js";
-import NavBar from "./components/Navbar/Navbar.js";
-import Footer from "./components/Footer/Footer.js";
+import Dashboard from './components/Dashboard/Dashboard.js';
+import NavBar from './components/Navbar/Navbar.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
 	return (
 		<div className='App'>
 			<Route exact path='/' component={RestaurantLoginPage} />
 			<Route path='/register' component={RestaurantCreateAccountPage} />
-      		<Route path='/dashboard' component={NavBar} />
-			  <Route path="/dashboard" component={Dashboard} />
+			<Route path='/dashboard' component={NavBar} />
+			<Route path='/dashboard' component={Dashboard} />
 			<Route path='/dashboard' component={Footer} />
 		</div>
 	);
