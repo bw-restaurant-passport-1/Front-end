f7ad04c5ac9a051b42afa0363da9694b2c1d7e26
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-01-07 21:56:29 -0500

    restaruantinfo comp made

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -3,13 +3,13 @@ import RestaurantLoginPage from './components/accounts/restaurantLoginPage';
 import RestaurantCreateAccountPage from './components/accounts/restaurantCreateAccountPage';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Dashboard from "./components/Dashboard/Dashboard.js";
-import NavBar from "./components/Navbar/Navbar.js";
-import Footer from "./components/Footer/Footer.js";
-import RestaurantList from "./components/RestaurantList/RestaurantList.js";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop.js";
-import AddRestaurant from "./components/AddRestaurant/AddRestaurant.js";
-
+import Dashboard from './components/Dashboard/Dashboard.js';
+import NavBar from './components/Navbar/Navbar.js';
+import Footer from './components/Footer/Footer.js';
+import RestaurantList from './components/RestaurantList/RestaurantList.js';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.js';
+import AddRestaurant from './components/AddRestaurant/AddRestaurant.js';
+import RestaurantInfo from './components/RestaurantInfo/RestaurantInfo';
 
 function App() {
 	return (
@@ -17,15 +17,16 @@ function App() {
 			<ScrollToTop />
 			<Route exact path='/' component={RestaurantLoginPage} />
 			<Route path='/register' component={RestaurantCreateAccountPage} />
-      		<Route path='/dashboard' component={NavBar} />
+			<Route path='/dashboard' component={NavBar} />
 
-			<Route exact path="/dashboard" component={Dashboard} />
+			<Route exact path='/dashboard' component={Dashboard} />
 
-			<Route path="/dashboard/restaurants" component={RestaurantList} />
-			
-			<Route path="/dashboard/add-restaurant" component={AddRestaurant} />
+			<Route path='/dashboard/restaurants' component={RestaurantList} />
+
+			<Route path='/dashboard/add-restaurant' component={AddRestaurant} />
 
 			<Route path='/dashboard' component={Footer} />
+			<Route path='dashboard/restaurant/info' component={RestaurantInfo} />
 		</div>
 	);
 }
