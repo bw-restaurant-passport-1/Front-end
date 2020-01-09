d72d9b6d3494f6bcb98c926cd1f8ae6e64a1b55c
adrianbparra <adrianplane12@gmail.com>
2020-01-08 20:46:07 -0700

    changed rest card to display data correctly since reviews are fetched seperatly

--- restaurant-passport/src/components/RestaurantsDashboard/RestaurantsDashboard.js
+++ restaurant-passport/src/components/RestaurantsDashboard/RestaurantsDashboard.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import RestaurantCard from "../RestaurantCard/RestaurantCard.js";
 import {Link} from "react-router-dom";
 
-const RestaurantsDashboard = () => {
+import { axiosWithAuth } from "../../utils/axiosWithAuth.js";
+
+const RestaurantsDashboard = (props) => {
 
     //Save the data for 3 of each section
+
+
+    const [restaurants , setRestaurants] = useState([]);
+    
+
+    // const [allRestaurants, setAllRest] = useState(props.restaurants);
+
+
+    const [allStamptedRest, setAllStampedRest] = useState([]);
+    const [allRecommendedRest, setAllRecommendedRest] = useState([]);
     
     const restaurant = {
-        restaurantName: "Michael’s Food Place",
-        restaurantPictureURL: "https://static.olocdn.net/menu/chilis/cdd356ec154236849bfe87c344ed0bde.jpg",
-        rating: 3,
-        been_here: true,
-        
+        "id": 1,
+		"restaurantName": "Chili's",
+		"streetAddress": "3917 S Gilbert Rd",
+		"city": "Gilbert",
+		"zipcode": "85296",
+		"phoneNumber": "(480) 812-4636",
+		"websiteURL": "www.chilis.com",
+		"restaurantPictureURL": "https://static.olocdn.net/menu/chilis/cdd356ec154236849bfe87c344ed0bde.jpg"
     };
 
+    // console.log(props)
+
+
+    useEffect(()=> {
+        axiosWithAuth().get('/api/restaurants').then((res)=> {
+            console.log(res);
+            setRestaurants(res.data);
+            
+        },(error) =>{
+            console.log(error);
+        })
+
+ 
+
+
+    },[])
+
+    const loadingRest = () => {
+        return (
+            <div>loading</div>
+        )
+    }
+
+    console.log(restaurants)
+
+    
 
     return (
 
@@ -26,9 +67,14 @@ const RestaurantsDashboard = () => {
                 </div>
                 
                 <div className="restaurants_container">
-                    <RestaurantCard restaurant= {restaurant}/>
-                    <RestaurantCard restaurant= {restaurant}/>
-                    <RestaurantCard restaurant= {restaurant}/>
+                    {/* DO a loop to show only 3 items here */}
+                    
+
+                    
+                    {/* <RestaurantCard restaurant= {restaurant}/> */}
+                    <RestaurantCard restaurant= {restaurants[0]}/>
+                    <RestaurantCard restaurant= {restaurants[1]}/>
+                    <RestaurantCard restaurant= {restaurants[3]}/>
                 </div>
                 
             </div>
